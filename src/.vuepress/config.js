6f74a6596a151cf303ba2eca7432fe384487f729
mcenahle <i@mn.edu.kg>
2026-01-21 12:48:52 +0800

    在网站加入旧博客网站存档的动态（1月21日）

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -55,6 +55,7 @@ module.exports = {
                 collapsable: false,
                 children: [
                     '/tongzhi/not-supporting-wechat-browser.html',
+                    '/tongzhi/old-blog-site-archived.html',
                 ]
             }
         ]
